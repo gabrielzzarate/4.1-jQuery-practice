2924292fc666f40d5e6dd574ccae644810f9490d
Gabriel Zarate <gabrielzzarate@gmail.com>
2016-02-22 18:22:30 -0500

    border change

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -9,8 +9,8 @@ console.log(allPanels);
 $('.accordion a').click(function(){
 	allPanels.slideUp();
 
-	$('.green').removeClass('green');
-  	$(this).parent().parent().addClass('green');
+	//$('.green').removeClass('green');
+  	//$(this).parent().parent().addClass('green');
 	$(this).parent().next().slideDown();
 	return false;
 });
