2cd30dddf0408bce8644d86e1333217577f2d963
Gabriel Zarate <gabrielzzarate@gmail.com>
2016-02-22 15:56:03 -0500

    first submission

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,25 @@
-alert('hello');
+var $ = require('jQuery');
+
+
+$(function(){
+
+var allPanels = $('.accordion ul').hide();
+console.log(allPanels);
+
+$('.accordion a').click(function(){
+	allPanels.slideUp();
+
+	$('.green').removeClass('green');
+  	$(this).parent().parent().addClass('green');
+	$(this).parent().next().slideDown();
+	return false;
+});
+
+
+  
+
+
+});
+
+
+//.animate()
